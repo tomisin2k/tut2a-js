d72ef4310f4ad5dd3e16e778e2f4f516b6071610
tomisin2k <103781056+tomisin2k@users.noreply.github.com>
2022-08-05 21:03:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function olodo() {
+  alert("Why you forget password, Odogwu?")
+}
+
+function bitch() {
+  alert("Welcome Bitch")
+}
+
+function btc() {
+  alert("Send 100btc to create a new account!")
+}
